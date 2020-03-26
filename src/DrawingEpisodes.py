3fecbb6c66ebde00ad07e48defe704c7d34be60e
michele <sload@smartload1>
2020-03-26 12:46:07 +0200

    Move datastructures from global to local

--- src/DrawingEpisodes.py
+++ src/DrawingEpisodes.py
@@ -22,47 +22,63 @@ def deleteFileIfExists(filename):
     else:
         print("The file does not exist")
 
+def find(name, path):
+    for root, dirs, files in os.walk(path):
+        if name in files or name in dirs:
+            return os.path.join(root, name)
+
+def determinePathToConfig():
+    user=os.getenv("HOME")
+    simcomloc = find("simcom", user)
+    confpath = simcomloc+"/config"
+    return confpath
+
+
 class randomEpisode:
     actual_seed=0
-    data = {}
-    data['Objects'] = []
-    NumberOfRocks = 0
-    VehiclePosition= PoseStamped()
+#    data = {}
+#    data['Objects'] = []
+#    NumberOfRocks = 0
+#    VehiclePosition= PoseStamped()
 
     def __init__(self, typeOfRand, newseed):
+        data = {}
+        data['Objects'] = []
+        NumberOfRocks = 0
+        VehiclePosition = PoseStamped()
         if newseed != 0:
-            self.actual_seed = random.seed(None,2)
+            actual_seed = random.seed(None,2)
         if typeOfRand == "verysimple":
-            self.NumberOfRocks = random.randint(1,10)
+            NumberOfRocks = random.randint(1,10)
         else:
-            self.NumberOfRocks = random.randint(1,10)
-        self.VehiclePosition.pose.position.x = random.uniform(0,500)
-        self.VehiclePosition.pose.position.y = 0
-        self.VehiclePosition.pose.position.z = random.uniform(0,500)
+            NumberOfRocks = random.randint(1,10)
+        VehiclePosition.pose.position.x = random.uniform(0,500)
+        VehiclePosition.pose.position.y = 0
+        VehiclePosition.pose.position.z = random.uniform(0,500)
         euler_orient = Vector3()
         euler_orient.x = 0
         euler_orient.y = random.uniform(-pi,pi)
         euler_orient.z = 0 #random.uniform(-pi,pi)
         quat_orient = toRW.euler_to_quaternion(euler_orient.x, euler_orient.y, euler_orient.z)
-        self.VehiclePosition.pose.orientation.x = quat_orient[0] #random.uniform(-1,1)
-        self.VehiclePosition.pose.orientation.y = quat_orient[1] #random.uniform(-1,1)
-        self.VehiclePosition.pose.orientation.z = quat_orient[2] #random.uniform(-1,1)
-        self.VehiclePosition.pose.orientation.w = quat_orient[3] #random.uniform(-1,1)
-        self.data['Objects'].append({
+        VehiclePosition.pose.orientation.x = quat_orient[0] #random.uniform(-1,1)
+        VehiclePosition.pose.orientation.y = quat_orient[1] #random.uniform(-1,1)
+        VehiclePosition.pose.orientation.z = quat_orient[2] #random.uniform(-1,1)
+        VehiclePosition.pose.orientation.w = quat_orient[3] #random.uniform(-1,1)
+        data['Objects'].append({
             'Name': 'BobCat',
             'Id': 'BobCat',
             'Position':
                 {
-                    'x': self.VehiclePosition.pose.position.x,
-                    'y': self.VehiclePosition.pose.position.y,
-                    'z': self.VehiclePosition.pose.position.z
+                    'x': VehiclePosition.pose.position.x,
+                    'y': VehiclePosition.pose.position.y,
+                    'z': VehiclePosition.pose.position.z
                 },
             'Rotation':
                 {
-                    'x': self.VehiclePosition.pose.orientation.x,
-                    'y': self.VehiclePosition.pose.orientation.y,
-                    'z': self.VehiclePosition.pose.orientation.z,
-                    'w': self.VehiclePosition.pose.orientation.w
+                    'x': VehiclePosition.pose.orientation.x,
+                    'y': VehiclePosition.pose.orientation.y,
+                    'z': VehiclePosition.pose.orientation.z,
+                    'w': VehiclePosition.pose.orientation.w
                 },
             'Scale':
                 {
@@ -72,20 +88,20 @@ class randomEpisode:
                  }
         })
 
-        BobcatX = self.VehiclePosition.pose.position.x
-        BobcatZ = self.VehiclePosition.pose.position.z
+        BobcatX = VehiclePosition.pose.position.x
+        BobcatZ = VehiclePosition.pose.position.z
         XMin = BobcatX - 1
         XMax = BobcatX + 1
         ZMin = BobcatZ - 1.5
         ZMax = BobcatZ + 1.5
-        for i in range(self.NumberOfRocks):
+        for i in range(NumberOfRocks):
             id = (i+1).__str__()
             eulerRot = Vector3()
             eulerRot.x = 0
             eulerRot.y = random.uniform(-pi, pi)
             eulerRot.z = 0 #random.uniform(-pi, pi)
             quatRot = toRW.euler_to_quaternion(eulerRot.x, eulerRot.y, eulerRot.z)
-            self.data['Objects'].append({
+            data['Objects'].append({
                 'Name': 'Rock',
                 'Id': id,
                 'Position':
@@ -108,50 +124,56 @@ class randomEpisode:
                         "z": 0.1
                     }
             })
-        deleteFileIfExists('/home/graphics/ws/interfaces/src/simcom/config/InitialScene.json')
-        with open('/home/graphics/ws/interfaces/src/simcom/config/InitialScene.json', 'w') as outfile:
-            json.dump(self.data, outfile)
+ #       deleteFileIfExists('/home/sload/ws/interfaces/src/simcom/config/InitialScene.json')
+        filepath = determinePathToConfig()+"/InitialScene.json"
+        with open(filepath, 'w') as outfile:
+            json.dump(data, outfile)
 
 class recorderEpisode:
-    actual_seed=0
-    data = {}
-    data['Objects'] = []
-    NumberOfRocks = 0
-    VehiclePosition= PoseStamped()
+    # actual_seed=0
+    # data = {}
+    # data['Objects'] = []
+    # NumberOfRocks = 0
+    # VehiclePosition= PoseStamped()
 
     def __init__(self, newseed):
+        actual_seed = 0
+        data = {}
+        data['Objects'] = []
+        NumberOfRocks = 0
+        VehiclePosition = PoseStamped()
         if newseed != 0:
-            self.actual_seed = random.seed(None,2)
+            actual_seed = random.seed(None,2)
 
-        self.NumberOfRocks = 2
+        NumberOfRocks = 2
 
-        self.VehiclePosition.pose.position.x = 250
-        self.VehiclePosition.pose.position.y = 0
-        self.VehiclePosition.pose.position.z = 250
+        VehiclePosition.pose.position.x = 250
+        VehiclePosition.pose.position.y = 0
+        VehiclePosition.pose.position.z = 250
         euler_orient = Vector3()
         euler_orient.x = 0
         euler_orient.y = random.uniform(-pi,pi)
         euler_orient.z = 0 #random.uniform(-pi,pi)
         quat_orient = toRW.euler_to_quaternion(euler_orient.x, euler_orient.y, euler_orient.z)
-        self.VehiclePosition.pose.orientation.x = quat_orient[0] #random.uniform(-1,1)
-        self.VehiclePosition.pose.orientation.y = quat_orient[1] #random.uniform(-1,1)
-        self.VehiclePosition.pose.orientation.z = quat_orient[2] #random.uniform(-1,1)
-        self.VehiclePosition.pose.orientation.w = quat_orient[3] #random.uniform(-1,1)
-        self.data['Objects'].append({
+        VehiclePosition.pose.orientation.x = quat_orient[0] #random.uniform(-1,1)
+        VehiclePosition.pose.orientation.y = quat_orient[1] #random.uniform(-1,1)
+        VehiclePosition.pose.orientation.z = quat_orient[2] #random.uniform(-1,1)
+        VehiclePosition.pose.orientation.w = quat_orient[3] #random.uniform(-1,1)
+        data['Objects'].append({
             'Name': 'BobCat',
             'Id': 'BobCat',
             'Position':
                 {
-                    'x': self.VehiclePosition.pose.position.x,
-                    'y': self.VehiclePosition.pose.position.y,
-                    'z': self.VehiclePosition.pose.position.z
+                    'x': VehiclePosition.pose.position.x,
+                    'y': VehiclePosition.pose.position.y,
+                    'z': VehiclePosition.pose.position.z
                 },
             'Rotation':
                 {
-                    'x': self.VehiclePosition.pose.orientation.x,
-                    'y': self.VehiclePosition.pose.orientation.y,
-                    'z': self.VehiclePosition.pose.orientation.z,
-                    'w': self.VehiclePosition.pose.orientation.w
+                    'x': VehiclePosition.pose.orientation.x,
+                    'y': VehiclePosition.pose.orientation.y,
+                    'z': VehiclePosition.pose.orientation.z,
+                    'w': VehiclePosition.pose.orientation.w
                 },
             'Scale':
                 {
@@ -161,8 +183,8 @@ class recorderEpisode:
                  }
         })
 
-        BobcatX = self.VehiclePosition.pose.position.x
-        BobcatZ = self.VehiclePosition.pose.position.z
+        BobcatX = VehiclePosition.pose.position.x
+        BobcatZ = VehiclePosition.pose.position.z
         XMin = BobcatX - 5
         XMax = BobcatX + 5
         ZMin = BobcatZ - 5
@@ -174,7 +196,7 @@ class recorderEpisode:
         eulerRot.y = random.uniform(-pi, pi)
         eulerRot.z = 0 #random.uniform(-pi, pi)
         quatRot = toRW.euler_to_quaternion(eulerRot.x, eulerRot.y, eulerRot.z)
-        self.data['Objects'].append({
+        data['Objects'].append({
             'Name': 'Rock',
             'Id': id,
             'Position':
@@ -204,7 +226,7 @@ class recorderEpisode:
         eulerRot.y = random.uniform(-pi, pi)
         eulerRot.z = 0 #random.uniform(-pi, pi)
         quatRot = toRW.euler_to_quaternion(eulerRot.x, eulerRot.y, eulerRot.z)
-        self.data['Objects'].append({
+        data['Objects'].append({
             'Name': 'Rock',
             'Id': id,
             'Position':
@@ -227,10 +249,13 @@ class recorderEpisode:
                     "z": 0.1
                 }
         })
-        with open('/home/graphics/ws/interfaces/src/simcom/config/InitialScene.json', 'w') as outfile:
-            json.dump(self.data, outfile)
+        filepath = determinePathToConfig()+"/InitialScene.json"
+        with open(filepath,'w') as outfile:
+            json.dump(data, outfile)
 
 if __name__ == '__main__':
     for j in range(3):
-        scenario = randomEpisode("coucou",j)
+        scenario = recorderEpisode(j)
+        #scenario = randomEpisode("coucou",j)
+
 
